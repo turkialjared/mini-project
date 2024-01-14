6ea082395dab0284e6b06586d8180b8dab5edd07
turki almutairi <turkeyaljared@hotmail.com>
2024-01-14 15:32:44 +0300

    push

--- src/component.js/Usertrans.js
+++ src/component.js/Usertrans.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Usertrans = ({ amount, type }) => {
+const Usertrans = ({ amount, type, key }) => {
   return (
     <div
       style={{
--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,17 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { my } from "../api/auth";
 import Usertrans from "../component.js/Usertrans";
+import { useState } from "react";
 
 const Transactions = () => {
+  const dayjs = require("dayjs");
+  const [Type, setType] = useState("");
+  const [query, setquery] = useState("");
   const { data: transactions, isLoading } = useQuery({
     queryKey: ["transactions"],
     queryFn: my,
   });
+  const search = (e) => {
+    setquery(e.target.value);
+  };
+
+  const search2 = (e) => {
+    setType(e.target.value);
+  };
 
   const mytransaction = transactions
-    ?.filter((trans) => trans.type)
-    .map((trans) => <Usertrans amount={trans.amount} type={trans.type} />);
-  console.log(transactions);
+    ?.filter((trans) => (Type ? trans.type === Type : true))
+    ?.filter((trans) =>
+      query
+        ? trans.description.toLowerCase().includes(query.toLowerCase())
+        : true
+    )
+    ?.map((trans) => (
+      <Usertrans key={trans.id} amount={trans.amount} type={trans.type} />
+    ));
   return (
     <>
       {isLoading ? (
@@ -29,6 +46,26 @@ const Transactions = () => {
               justifyContent: "space-evenly",
             }}
           >
+            <div className="input-group rounded">
+              <input
+                onChange={search}
+                type="search"
+                className="form-control rounded"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+              />
+            </div>
+            <br />
+            Type:
+            <select className="form-select" onChange={search2}>
+              <option value="" selected>
+                All
+              </option>
+              <option value="deposit">deposit</option>
+              <option value="withdraw">withdraw</option>
+              <option value="transfer">transfer</option>
+            </select>
             {mytransaction}
           </h3>
         </>
